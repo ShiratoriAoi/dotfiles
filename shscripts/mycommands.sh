79d8f4b632dfb1d1cfa6b20c31a8c90f53cffb43
Aoi SHIRATORI <aoy.shiratori@gmail.com>
2022-05-31 19:21:56 +0900

    fix: make chgit more better

--- shscripts/mycommands.sh
+++ shscripts/mycommands.sh
@@ -1,11 +1,24 @@
 
 source $HOME_DOTFILES/shscripts/git_status.sh
 
-REPO1=`find $HOME/Dev/Projects/Developing -maxdepth 1 -mindepth 1 -type d | tr "\n" " "`
-REPO2=`find $HOME/Dev/Projects/MyLibrary  -maxdepth 1 -mindepth 1 -type d | tr "\n" " "`
-REPO3="$HOME/Dev/dotfiles"
-STR="$REPO1 $REPO2 $REPO3"
+function chgit() {
+    local REPO1=`find $HOME/Dev/Projects/Developing -maxdepth 1 -mindepth 1 -type d | tr "\n" " "`
+    local REPO2=`find $HOME/Dev/Projects/MyLibrary  -maxdepth 1 -mindepth 1 -type d | tr "\n" " "`
+    local REPO3="$HOME/Dev/dotfiles"
+    local STR="$REPO1 $REPO2 $REPO3"
 
-alias chgit="check_git_status $STR"
-alias chgitf="check_git_status_with_fetch $STR"
-unset REPO1 REPO2 REPO3 STR
+    if [ $# -eq 0 ] ; then
+        #=→空白の箇所で単語分割
+        check_git_status ${=STR}
+    elif [ "$1" = "-h" ] ; then
+        echo "chgit"
+        echo "chgit fetch"
+        echo "chgit cd <dir>"
+        echo "chgit -h"
+    elif [ "$1" = "cd" ] ; then
+        cd `echo $STR | tr " " "\n" | grep "$2"`
+    elif [ "$1" = fetch ] ; then
+        check_git_status_with_fetch ${=STR}
+    fi 
+}
+
