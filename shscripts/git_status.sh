f27698f3c4942646bf5e4a5f28f2cbb596046cca
Aoi SHIRATORI <aoy.shiratori@gmail.com>
2022-05-31 18:23:46 +0900

    add: shell command for check git status.

--- shscripts/git_status.sh
+++ shscripts/git_status.sh
@@ -6,11 +6,15 @@ function check_one_git_status() {
         local dotgit="$1/.git"
         if [ -e "$dotgit" ] ; then
             echo -e "\e[37;44;5m `basename "$1"` \e[m"
-            echo "    $1"
-            git status | sed 's/^/    /' | grep --color=auto -e '$' -e 'Untracked files:'
+            echo "  $1"
+            if [ $2 = "fetch" ] ; then
+                git -C $1 fetch
+            fi
+            git -C $1 status -sb 
             echo "----------------------------------------------------------"
         else
             echo "dir found, .git not exist: $dotgit"
+            echo "----------------------------------------------------------"
         fi
     else
         #directory sonzai sinai.
@@ -21,10 +25,15 @@ function check_one_git_status() {
 function check_git_status() {
     echo "----------------------------------------------------------"
     while [ $# -gt 0 ] ; do
-        check_one_git_status $1
+        check_one_git_status $1 "n"
         shift
     done
-    echo "----------------------------------------------------------"
 }
 
-
+function check_git_status_with_fetch() {
+    echo "----------------------------------------------------------"
+    while [ $# -gt 0 ] ; do
+        check_one_git_status $1 "fetch"
+        shift
+    done
+}
--- shscripts/mycommands.sh
+++ shscripts/mycommands.sh
@@ -1,5 +1,11 @@
 
 source $HOME_DOTFILES/shscripts/git_status.sh
-alias chgit='check_git_status ~/Dev/dotfiles'
 
-#find dotfiles -maxdepth 1 -mindepth 1 -type d
+REPO1=`find $HOME/Dev/Projects/Developing -maxdepth 1 -mindepth 1 -type d | tr "\n" " "`
+REPO2=`find $HOME/Dev/Projects/MyLibrary  -maxdepth 1 -mindepth 1 -type d | tr "\n" " "`
+REPO3="$HOME/Dev/dotfiles"
+STR="$REPO1 $REPO2 $REPO3"
+
+alias chgit="check_git_status $STR"
+alias chgitf="check_git_status_with_fetch $STR"
+unset REPO1 REPO2 REPO3 STR
